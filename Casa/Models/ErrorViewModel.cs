37432e519bdf4c7dc1917c1d1597636d8ff05787
Dragon <luismanco7@gmail.com>
2025-04-23 11:46:34 -0500

    casa inicial

---
+++ Casa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Casa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
